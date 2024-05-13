3629febf6bc055fc3e4c650271b8308ff6baa30b
DanyloPlotskykh <danylo.plotskykh.ki.2022@lpnu.ua>
2024-05-13 23:15:51 +0300

    add gui for client

--- build/_client/CMakeFiles/test_project.dir/test_project_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-_client/CMakeFiles/test_project.dir/test_project_autogen/mocs_compilation.cpp.o: \
- /home/user/p/TextCommunicationGUI/build/_client/test_project_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
